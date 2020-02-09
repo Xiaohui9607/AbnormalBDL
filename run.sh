ef2122ca91acab1c57439d674be3ff746d201a0a
golfgang <joy.cxh0726@gmail.com>
2020-02-09 12:54:31 +0800

    m

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-python train.py --name M_pairs_0 --abnormal_class 0
-python train.py --name M_pairs_1 --abnormal_class 1
-python train.py --name M_pairs_2 --abnormal_class 2
-python train.py --name M_pairs_3 --abnormal_class 3
-python train.py --name M_pairs_4 --abnormal_class 4
-python train.py --name M_pairs_5 --abnormal_class 5
-python train.py --name M_pairs_6 --abnormal_class 6
-python train.py --name M_pairs_7 --abnormal_class 7
+#python train.py --name M_pairs_0 --abnormal_class 0
+#python train.py --name M_pairs_1 --abnormal_class 1
+#python train.py --name M_pairs_2 --abnormal_class 2
+#python train.py --name M_pairs_3 --abnormal_class 3
+#python train.py --name M_pairs_4 --abnormal_class 4
+#python train.py --name M_pairs_5 --abnormal_class 5
+#python train.py --name M_pairs_6 --abnormal_class 6
+#python train.py --name M_pairs_7 --abnormal_class 7
 python train.py --name M_pairs_8 --abnormal_class 8
 python train.py --name M_pairs_9 --abnormal_class 9
