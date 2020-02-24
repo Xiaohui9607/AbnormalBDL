7db447780f364de00b9fd051cb3ab7f220466adb
golfgang <joy.cxh0726@gmail.com>
2020-02-23 19:55:42 -0500

    plot update

--- models/evaluate.py
+++ models/evaluate.py
@@ -16,8 +16,7 @@ from scipy.optimize import brentq
 from scipy.interpolate import interp1d
 import matplotlib.pyplot as plt
 from matplotlib import rc
-# rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
-# rc('text', usetex=False)
+
 
 
 def evaluate(labels, scores, metric='roc'):
